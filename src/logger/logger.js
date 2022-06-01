c2ab3a25d0cfdbb82e968ea843b021990f97149d
linksandeep <106560sandeeppal@gmail.com>
2022-06-01 19:23:17 +0530

    my second project

---
+++ src/logger/logger.js
@@ -0,0 +1,6 @@
+const log = function(req ,res) {
+    res.send("Welcome to my application. I am sandeep and a part of FunctionUp Radon cohort  kk.")
+}
+
+
+module.exports.log = log
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const externalModule = require('./logger')
+const externalModule = require('../logger/logger')
 
 const router = express.Router();
 
@@ -10,21 +10,13 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
-});
+router.get('/test-me1',externalModule.log );
 
 router.get('/test-me2', function (req, res) {
     res.send('My third api!')
 });
 
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
 
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
 
 module.exports = router;
 // adding this comment for no reason
