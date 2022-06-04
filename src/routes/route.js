c8deab7a4b5470e0308b7659c60a1ef10bb1691c
linksandeep <106560sandeeppal@gmail.com>
2022-06-04 13:10:10 +0530

    Creating book data using Postman

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 
-router.get("/test-me", function (req, res) {
+router.get("/teest-m", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/books-i.e", UserController.createBook  )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/books-i.e2", UserController.getAllBook)
 
 module.exports = router;
